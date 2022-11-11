3a0de25593eff4eaa1fa5728508d9d2ad81409a3
Hsuan-Ting Lu <hsuan.ting.lu@nyu.edu>
2022-11-10 21:05:22 -0500

    Add sandboxed templating

--- scraper/html-scraper/tests/utils/mock/__init__.py
+++ scraper/html-scraper/tests/utils/mock/__init__.py
@@ -1,7 +1,8 @@
 import pathlib
 import jinja2
+import jinja2.sandbox
 
-env = jinja2.Environment(
+env = jinja2.sandbox.SandboxedEnvironment(
     loader=jinja2.FileSystemLoader(
         pathlib.PurePath(__file__).parent / "template"),
     autoescape=jinja2.select_autoescape,
