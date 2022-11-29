5b3e8f2807d6c573b3551eee8a6a89c9fb3504e5
Hsuan-Ting Lu <hsuan.ting.lu@nyu.edu>
2022-11-29 16:21:28 -0500

    Add class definitions to encapsulate jobs

--- scraper/html-scraper/scraper/scrape.py
+++ scraper/html-scraper/scraper/scrape.py
@@ -1,10 +1,45 @@
 """ Main scraper code
 """
+import functools
 import importlib.resources
 import itertools
 import random
-from scraper.perf import Timer
+import requests
 import scraper
+
+
+class Task:
+    # TODO: record self.response.elapsed info for scheduling
+
+    class Request:
+
+        def __init__(
+            self,
+            request: requests.Request,
+            timeout,
+            priority,
+        ):
+            self.request = request
+            self.timeout = timeout
+            self.priority = priority  # additional priority for scheduler to use (e.g. can be used to crawl shallow pages first or deep pages first)
+            # TODO: consider doing "request-fingerprinting" to filter out duplicate requests or cache them
+        # TODO: log timestamp before sending
+
+    def __init__(
+        self,
+        request: requests.Request,
+        *,
+        timeout=60,
+        priority=1,
+        #referer=None, # TODO: move this to a middleware and log it in self.metadata
+        #root_request_hostname=None,  # TODO: move this to DropSecondLevelThirdPartyMiddleware and log it in self.metadata
+    ):
+        self.request = self.Request(request, timeout, priority)
+        self.response = None
+        self.metadata = {}
+
+        # TODO: in some middleware: if is root request (no previous value set), set depth=1, root_hostname=hostname
+
 
 
 class StartURLs:
@@ -37,19 +72,30 @@ class StartURLs:
         return repr(self.urls)
 
 
+class Scraper:
+
+    def __init__(self, start_urls: StartURLs, *args, **kwargs):
+        pass
+
+
 def scrape():
 
-    StartURLs(
-        scraper.target.extract(
-            data_dir=importlib.resources.files(scraper.targets),
-            usecols=["Website"],
-            dtype={"Website": "string"},
-        ))
-    exit()
+    Scraper(
+        StartURLs(
+            scraper.target.extract(
+                data_dir=importlib.resources.files(scraper.targets),
+                usecols=["Website"],
+                dtype={"Website": "string"},
+            )),
+        scraper.Crawler(),
+        scraper.Downloader(),
+    )
 
     # TODO: move these to performance profiling code
+    """
     fmt_str = lambda action_name: action_name + ":\n\t{:0.4f} seconds"
 
+    Timer = scraper.perf.Timer
     with Timer(fmt_str("Read nces")):
         nces_df = pd.read_excel(nces_file)
     with Timer(fmt_str("Read k12")):
@@ -63,6 +109,7 @@ def scrape():
 
     print(f"{'-' * 12}")
     print(f"number of targets: {len(district_url_set)}")
+    """
 
 
 if __name__ == "__main__":
