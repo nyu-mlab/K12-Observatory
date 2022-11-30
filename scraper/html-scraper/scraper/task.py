b44934630abdbe94bcb34c929b9705a1011ce0c2
Hsuan-Ting Lu <hsuan.ting.lu@nyu.edu>
2022-11-30 13:06:47 -0500

    Refactor ```Task```
    merge member class Task.Request into Task class

--- scraper/html-scraper/scraper/task.py
+++ scraper/html-scraper/scraper/task.py
@@ -4,23 +4,6 @@ import requests
 
 
 class Task:
-    # TODO: record self.response.elapsed info for scheduling
-
-    class Request:
-
-        def __init__(
-            self,
-            request: requests.Request,
-            timeout,
-            priority,
-        ):
-            self.request = request
-            self.timeout = timeout
-            self.priority = priority  # additional priority for scheduler to use (e.g. can be used to crawl shallow pages first or deep pages first)
-            # TODO: consider doing "request-fingerprinting" to filter out duplicate requests or cache them
-            # TODO: can designate outgoing interface (binded address) or should we delegate to scheduling stage?
-
-        # TODO: log timestamp before sending
 
     def __init__(
         self,
@@ -29,11 +12,20 @@ class Task:
         timeout=60,
         priority=1,
         #referer=None, # TODO: move this to a middleware and log it in self.metadata
-        #root_request_hostname=None,  # TODO: move this to DropSecondLevelThirdPartyMiddleware and log it in self.metadata
     ):
-        self.request = self.Request(request, timeout, priority)
-        self.response = None
-        self.metadata = {}
+        self.request = request
+        self.timeout = timeout
+        self.priority = priority  # additional priority for scheduler to use (e.g. can be used to crawl shallow pages first or deep pages first)
+        # TODO: consider doing "request-fingerprinting" to filter out duplicate requests or cache them
+        # TODO: can designate outgoing interface (binded address) or should we delegate to scheduling stage?
+        # TODO: log timestamp before sending
+        # TODO: record self.response.elapsed info for scheduling
+
+        self.response: requests.Response = None
+        self.results: list[Task] = []
+        self.metadata = {
+        }  # TODO: consider dropping "metadata" and use "self.<var>" directly for middleware data instead, since member variables does not need to be explicitly defined at __init__ anyway
 
         # TODO: in some middleware: if is root request (no previous value set), set depth=1, root_hostname=hostname
 
+    # TODO: sort all property methods
