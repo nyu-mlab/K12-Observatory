526fbd9cc2671a48786bfef206d63c809e4b3e24
Hsuan-Ting Lu <hsuan.ting.lu@nyu.edu>
2022-12-03 21:55:43 -0500

    Add parallelization for middleware execution
    Exploit parallelizable work with DAGs

--- scraper/html-scraper/scraper/crawler.py
+++ scraper/html-scraper/scraper/crawler.py
@@ -1,22 +1,29 @@
 """ tries to understand what the page is about
 including process and analyze textual content + key content tags and attributes
 """
+import graphlib
+import multiprocessing as mp
+
 import scraper.task
 from scraper import crawler_middleware
 
 
-default_middleware = (
-    crawler_middleware.BinaryContent,
-    crawler_middleware.Depth,
-    crawler_middleware.ThirdParty,
-    crawler_middleware.Referer,
-)
+default_middleware = graphlib.TopologicalSorter({
+    crawler_middleware.BinaryContent(): (),
+    crawler_middleware.Depth(): (),
+    crawler_middleware.ThirdParty(): (),
+    crawler_middleware.Referer(): (),
+})
 
 
 class Crawler:
     """Crawler"""
 
-    def __init__(self, n_worker=1, middleware=default_middleware):
+    def __init__(self,
+                 n_worker: int = None,
+                 middleware: graphlib.TopologicalSorter = default_middleware):
+        middleware.prepare()
+        self.middleware = middleware
         pass
 
     def parse(self, task: scraper.task.Task):
--- scraper/html-scraper/scraper/downloader.py
+++ scraper/html-scraper/scraper/downloader.py
@@ -1,22 +1,28 @@
 """ visit (or "crawl") the page to find out what's on it
 """
+import graphlib
+import multiprocessing as mp
+
 import scraper.task
 from scraper import downloader_middleware
 
-default_middleware = (
-    downloader_middleware.BinaryContent,
-    downloader_middleware.HttpError,
-    #downloader_middleware.HttpProxy,  # XXX: do we need this?
-    downloader_middleware.JsCrawl,
-    #downloader_middleware.UserAgent,  # XXX: do we need this?
-)
+default_middleware = graphlib.TopologicalSorter({
+    downloader_middleware.BinaryContent(): (),
+    downloader_middleware.HttpError(): (),
+    #downloader_middleware.HttpProxy(): (),  # XXX: do we need this?
+    downloader_middleware.JsCrawl(): (),
+    #downloader_middleware.UserAgent(): (),  # XXX: do we need this?
+})
 
 
 class Downloader:
     """Downloader"""
 
-    def __init__(self, n_worker=1, middleware=default_middleware):
-        pass
+    def __init__(self,
+                 n_worker: int = None,
+                 middleware: graphlib.TopologicalSorter = default_middleware):
+        middleware.prepare()
+        self.middleware = middleware
 
     def process(self, task: scraper.task.Task):
         if task.metadata.get("drop"):
