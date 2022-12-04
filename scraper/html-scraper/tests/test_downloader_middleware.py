3ad64752f3c32e6d762075ca70c31b20cf2b8b73
Hsuan-Ting Lu <hsuan.ting.lu@nyu.edu>
2022-12-04 18:41:47 -0500

    Add tests for the downloader middleware base class

--- scraper/html-scraper/tests/test_downloader_middleware.py
+++ scraper/html-scraper/tests/test_downloader_middleware.py
@@ -1,15 +1,36 @@
 import pytest
-
+import requests
 import scraper.downloader_middleware as middleware
 import scraper.task
 
 
 # TODO: parametrize fixture with different tasks
-@pytest.fixture(scope="function", params=["""TODO:"""])
-def task():
-    _: scraper.task.Task
-    # TODO: monkeypatch requests.Response
-    pass
+@pytest.fixture
+def basic_task():
+
+    def _basic_task():
+        url = "http://localhost/resource"
+        task = scraper.task.Task(requests.Request(url=url))
+        response = requests.Response()
+        response.url = url
+        task.response = response
+
+        return task
+
+    return _basic_task
+
+
+class TestMiddlewareBaseClass:
+
+    def test_base_class_is_abstract(self):
+        with pytest.raises(TypeError):
+            middleware.Middleware()
+
+    def test_duck_type_methods(self):
+        # exists an abstract classmethod function called "process"
+        assert callable(middleware.Middleware.process)
+        assert "process" in middleware.Middleware.__abstractmethods__
+        middleware.Middleware.process(None)
 
 
 class TestBinaryContentMiddleware:
